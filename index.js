61727fd881b8ea4b332ecec4b23c1464b4313487
ilefM <ilefikhelefdev1@gmail.com>
2023-04-25 03:15:11 -0400

    first try of a sort idk the name

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 function generate() {
-	const nbDivs = document.getElementById("nbElem").value;
+  const nbDivs = document.getElementById("nbElem").value;
 
-	for (let i = 0; i < 2; i++) {
-		const randomHeight = Math.floor(Math.random() * (500 - 1 + 1)) + 0;
+  for (let i = 0; i < 4; i++) {
+    const randomHeight = Math.floor(Math.random() * (500 - 1 + 1)) + 0;
 
-		const div = document.createElement("div");
-		div.id = `${i}`;
-		div.style.height = `${randomHeight}px`;
+    const div = document.createElement("div");
+    div.id = `${i}`;
+    div.style.height = `${randomHeight}px`;
 
-		document.getElementById("sorting-container").appendChild(div);
-	}
+    document.getElementById("sorting-container").appendChild(div);
+  }
 
-	document.querySelector("#generateButton").disabled = true;
+  document.querySelector("#generateButton").disabled = true;
 }
 
 function reset() {
-	const children = document.querySelectorAll("#sorting-container > *");
-	for (const child of children) {
-		child.remove();
-	}
+  const children = document.querySelectorAll("#sorting-container > *");
+  for (const child of children) {
+    child.remove();
+  }
 
-	document.querySelector("#generateButton").disabled = false;
+  document.querySelector("#generateButton").disabled = false;
 }
 
-function start(){
-	const children = document.querySelectorAll("#sorting-container > *");
-	
-	for(let i = 0; i < children.length(); i++){
-		if(children[i].style.height > children[i+1].style.height){
-			
-		}
-	}
+function start() {
+  const bars = document.querySelectorAll("#sorting-container > *");
+
+  for (let i = 0; i < bars.length - 1; i++) {
+    if (bars[i].style.height > bars[i + 1].style.height) {
+      const smallBarHeight = bars[i + 1].style.height;
+      bars[i + 1].style.height = bars[i].style.height;
+      bars[i].style.height = smallBarHeight;
+    }
+  }
 }
