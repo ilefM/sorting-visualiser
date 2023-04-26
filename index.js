d3fac1946ad1347c95fb190066a45e5569c329d8
ilefM <ilefikhelefdev1@gmail.com>
2023-04-26 00:11:26 -0400

    finishing the UI aspect (I wish I dont have to mess with it again)

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 function generate() {
-  const nbDivs = document.getElementById("nbElem").value;
+	const nbDivs = document.getElementById("nbElem").value;
 
-  for (let i = 0; i < 4; i++) {
-    const randomHeight = Math.floor(Math.random() * (500 - 1 + 1)) + 0;
+	for (let i = 0; i < nbDivs; i++) {
+		const randomHeight = Math.floor(Math.random() * (500 - 1 + 1)) + 1;
 
-    const div = document.createElement("div");
-    div.id = `${i}`;
-    div.style.height = `${randomHeight}px`;
+		const div = document.createElement("div");
+		div.id = `${i}`;
+		div.style.height = `${randomHeight}px`;
 
-    document.getElementById("sorting-container").appendChild(div);
-  }
+		document.getElementById("sorting-container").appendChild(div);
+	}
 
-  document.querySelector("#generateButton").disabled = true;
+	document.querySelector("#generateButton").disabled = true;
+	document.querySelector("#startButton").disabled = false;
+	document.querySelector("#stopButton").disabled = false;
 }
 
 function reset() {
-  const children = document.querySelectorAll("#sorting-container > *");
-  for (const child of children) {
-    child.remove();
-  }
+	const children = document.querySelectorAll("#sorting-container > *");
+	for (const child of children) {
+		child.remove();
+	}
 
-  document.querySelector("#generateButton").disabled = false;
+	document.querySelector("#generateButton").disabled = false;
 }
 
 function start() {
-  const bars = document.querySelectorAll("#sorting-container > *");
+	const bars = document.querySelectorAll("#sorting-container > *");
+	bubbleSort(bars);
+}
 
-  for (let i = 0; i < bars.length - 1; i++) {
-    if (bars[i].style.height > bars[i + 1].style.height) {
-      const smallBarHeight = bars[i + 1].style.height;
-      bars[i + 1].style.height = bars[i].style.height;
-      bars[i].style.height = smallBarHeight;
-    }
-  }
+function bubbleSort(bars) {
+	for (let i = 0; i < bars.length - 1; i++) {
+		if (bars[i].style.height > bars[i + 1].style.height) {
+			const smallBarHeight = bars[i + 1].style.height;
+			bars[i + 1].style.height = bars[i].style.height;
+			bars[i].style.height = smallBarHeight;
+		}
+	}
 }
