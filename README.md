96067c0f52a6a3d92cb84e8382b243c7ce234223
ilefM <ilefikhelefdev1@gmail.com>
2023-04-26 04:28:49 -0400

    testing the algorithm with animation

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ This project is a simple sorting algorithms visualiser
 - [x] Generate a number of collumns (or bars) with all a random heigh.
 - [x] Reset all the configuration.
 - [ ] Choose a sorting algorithm from an input list of algorithms.
-- [ ] Start the execution of an algorithm.
+      (At this moment, only the bubble sort is available)
+- [x] Start the execution of an algorithm.
 - [ ] Pause the execution at any time (the bars state are paused).
 - [ ] Print the execution time of the algorithm.
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function generate() {
-	const nbDivs = document.getElementById("nbElem").value;
+	const nbDivs = document.querySelector(".nbBarsInput").value;
 
 	for (let i = 0; i < nbDivs; i++) {
 		const randomHeight = Math.floor(Math.random() * (500 - 1 + 1)) + 1;
@@ -8,34 +8,58 @@ function generate() {
 		div.id = `${i}`;
 		div.style.height = `${randomHeight}px`;
 
-		document.getElementById("sorting-container").appendChild(div);
+		document.querySelector(".sorting-container").appendChild(div);
 	}
 
-	document.querySelector("#generateButton").disabled = true;
-	document.querySelector("#startButton").disabled = false;
-	document.querySelector("#stopButton").disabled = false;
+	document.querySelector(".generateButton").disabled = true;
+	document.querySelector(".startButton").disabled = false;
+	document.querySelector(".stopButton").disabled = false;
 }
 
 function reset() {
-	const children = document.querySelectorAll("#sorting-container > *");
+	const children = document.querySelectorAll(".sorting-container > *");
 	for (const child of children) {
 		child.remove();
 	}
 
-	document.querySelector("#generateButton").disabled = false;
+	document.querySelector(".generateButton").disabled = false;
+	document.querySelector(".startButton").disabled = true;
+	document.querySelector(".stopButton").disabled = true;
 }
 
 function start() {
-	const bars = document.querySelectorAll("#sorting-container > *");
+	const bars = document.querySelectorAll(".sorting-container > *");
 	bubbleSort(bars);
 }
 
-function bubbleSort(bars) {
-	for (let i = 0; i < bars.length - 1; i++) {
-		if (bars[i].style.height > bars[i + 1].style.height) {
-			const smallBarHeight = bars[i + 1].style.height;
-			bars[i + 1].style.height = bars[i].style.height;
-			bars[i].style.height = smallBarHeight;
+function cssToNumber(css) {
+	return Number(css.substring(0, css.length - 2));
+}
+
+function sleep() {
+	return new Promise((r) => setTimeout(r, 4));
+}
+
+async function bubbleSort(bars) {
+	for (let i = 1; i < bars.length; i++) {
+		for (let j = 0; j < bars.length - i; j++) {
+			const currentH = cssToNumber(bars[j].style.height);
+			const nextH = cssToNumber(bars[j + 1].style.height);
+
+			bars[j].style.backgroundColor = "#e63946";
+
+			if (currentH > nextH) {
+				// bars[j].style.backgroundColor = "#e63946";
+				// bars[j + 1].style.backgroundColor = "#1d3557";
+				let smallH;
+				smallH = bars[j + 1].style.height;
+				bars[j + 1].style.height = bars[j].style.height;
+				bars[j].style.height = smallH;
+				// bars[j].style.backgroundColor = "#f1faee";
+				// bars[j + 1].style.backgroundColor = "#f1faee";
+			}
+			await sleep();
+			bars[j].style.backgroundColor = "#f1faee";
 		}
 	}
 }
